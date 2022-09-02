a8c3e1867444a8365c3e6feb69c33218cf8c9186
Eugene <eugene.multan@gmail.com>
2022-09-02 15:23:45 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-import { useSelector } from 'react-redux';
-import { useCurrentUserQuery } from 'store/APIs/userAPI';
-
-// import styled from 'styled-components';
 
 const Contacts = lazy(() => import('../../pages/ContactsPage/ContactsPage'));
 const Login = lazy(() => import('../../pages/LoginPage/LoginPage'));
@@ -11,32 +7,11 @@ const Register = lazy(() => import('../../pages/RegisterPage/RegisterPage'));
 const NotFound = lazy(() => import('../../pages/NotFoundPage/NotFoundPage'));
 const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
 
-// const StyledLink = styled(NavLink)`
-//   text-decoration: none;
-//   margin-left: 10px;
-//   font-size: 22px;
-//   color: black;
-//   &.active {
-//     color: blue;
-//   }
-// `;
+// con
 
 export const App = () => {
-  // const { token } = useSelector(state => state.user);
-  // useCurrentUserQuery(undefined, {
-  //   skip: !token,
-  // });
-  // console.log(token);
-
   return (
     <div>
-      {/* <header>
-        <nav>
-          <StyledLink to={'/contacts'}>Contacts</StyledLink>
-          <StyledLink to={'/users/login'}>Sign In</StyledLink>
-          <StyledLink to={'/users/register'}>Sign Up</StyledLink>
-        </nav>
-      </header> */}
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
