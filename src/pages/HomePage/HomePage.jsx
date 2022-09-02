767efbe6728c75402da6769b770ed251b9dbcc79
Eugene <eugene.multan@gmail.com>
2022-09-02 14:11:03 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,13 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <div>
-      Hello! Please <Link to={'users/login'}>Sign In</Link> or{' '}
-      <Link to={'/users/register'}>Sign Up</Link>
+      <h1>I congratulate you!</h1>
+      <p>
+        You are on the CONTACTS service. CONTACTS allows you to save and view
+        your phone contacts at any time convenient for you. Please{' '}
+        <Link to={'users/login'}>Sign In</Link> or{' '}
+        <Link to={'/users/register'}>Sign Up</Link> to get started.
+      </p>
     </div>
   );
 };
